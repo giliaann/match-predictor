6ace3a3eac24ecf1b93a7afcef5a3874b89d4bb1
Przemyslaw Mazurowski <przemyslaw.mazurowski@gmail.com>
2026-06-20 22:03:23 +0200

    feat: Add competitions app models - add Team, Competition, Match models - make migration to create corresponding tables

--- competitions/models.py
+++ competitions/models.py
@@ -1,3 +1,49 @@
 from django.db import models
+    
+class Team(models.Model):
 
-# Create your models here.
+    api_id = models.BigIntegerField(unique=True)
+    name = models.CharField(max_length=128)
+    code = models.CharField(max_length=16)
+    emblem = models.URLField()
+    
+    def __str__(self):
+        return self.name
+
+class Competition(models.Model):
+    
+    api_id = models.BigIntegerField(unique=True)
+    season_api_id = models.BigIntegerField()
+    name = models.CharField(max_length=128)
+    date_start = models.DateField()
+    date_finish = models.DateField()
+    emblem = models.URLField()
+
+    teams = models.ManyToManyField(Team, related_name='competitions', blank=True)
+
+    def __str__(self):
+        return f'{self.name} - {self.date_start.year}'
+
+
+class Match(models.Model):
+
+    api_id = models.BigIntegerField(unique=True)
+
+    status = models.CharField(max_length=32)
+    kickoff_time = models.DateTimeField()
+
+    competition = models.ForeignKey(Competition, on_delete=models.CASCADE, related_name='matches')
+
+    home_team = models.ForeignKey(Team, on_delete=models.CASCADE, related_name='home_matches')
+    away_team = models.ForeignKey(Team, on_delete=models.CASCADE, related_name='away_matches')
+
+    home_score = models.SmallIntegerField(null=True, blank=True)
+    away_score = models.SmallIntegerField(null=True, blank=True)
+
+    home_penalties = models.SmallIntegerField(null=True, blank=True)
+    away_penalties = models.SmallIntegerField(null=True, blank=True)
+
+    def __str__(self):
+        return f'{self.home_team} vs {self.away_team} - {self.competition}'
+
+
