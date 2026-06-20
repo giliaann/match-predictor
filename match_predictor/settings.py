0931c519307968de1133dba908e76ea0fde4f78c
Przemyslaw Mazurowski <przemyslaw.mazurowski@gmail.com>
2026-06-20 16:16:25 +0200

    feat: Add competitons app - add new django app

--- match_predictor/settings.py
+++ match_predictor/settings.py
@@ -35,6 +35,7 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    'competitions.apps.CompetitionsConfig',
     'users.apps.UsersConfig',
     'django.contrib.admin',
     'django.contrib.auth',
