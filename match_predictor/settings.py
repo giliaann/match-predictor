0279449a95974cc65b01a6669f46ecb45b8f7250
Przemyslaw Mazurowski <przemyslaw.mazurowski@gmail.com>
2026-06-20 13:51:27 +0200

    feat: Add users app - create user model - make first migration

--- match_predictor/settings.py
+++ match_predictor/settings.py
@@ -35,6 +35,7 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    'users.apps.UsersConfig',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
@@ -86,6 +87,8 @@ DATABASES = {
 
 # Password validation
 # https://docs.djangoproject.com/en/6.0/ref/settings/#auth-password-validators
+
+AUTH_USER_MODEL = 'users.User'
 
 AUTH_PASSWORD_VALIDATORS = [
     {
