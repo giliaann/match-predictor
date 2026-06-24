b431bc97a79e25dbc9f991d8b851cd3c835f6454
Przemyslaw Mazurowski <przemyslaw.mazurowski@gmail.com>
2026-06-24 15:39:53 +0200

    feat: Add leave competition view

--- competitions/urls.py
+++ competitions/urls.py
@@ -1,7 +1,8 @@
 from django.urls import path
-from .views import CompetitionListView, CompetitionDetailView
+from .views import CompetitionListView, CompetitionDetailView, CompetitionLeaveView
 
 urlpatterns = [
     path('', CompetitionListView.as_view(), name='competition_list'),
+    path('<str:code>/leave/', CompetitionLeaveView.as_view(), name='competition_leave'),
     path('<str:code>/', CompetitionDetailView.as_view(), name='competition_detail')
 ]
--- competitions/views.py
+++ competitions/views.py
@@ -1,7 +1,9 @@
 from django.shortcuts import render
-
-from django.views.generic import ListView, DetailView
+from django.shortcuts import get_object_or_404, redirect
+from django.views.generic import ListView, DetailView, View
 from .models import Competition
+from django.contrib import messages
+from django.contrib.auth.mixins import LoginRequiredMixin
 
 class CompetitionListView(ListView):
     model = Competition
@@ -31,3 +33,29 @@ class CompetitionDetailView(DetailView):
         context['matches'] = self.object.matches.all().order_by('kickoff_time')
         
         return context
+    
+
+class CompetitionLeaveView(LoginRequiredMixin, View):
+    def post(self, request, code, *args, **kwargs):
+        competition = get_object_or_404(Competition, code=code)
+        
+        competition_registration = get_object_or_404(
+            request.user.competition_registrations,
+            competition=competition
+        )
+        
+        is_admin_somewhere = competition_registration.leaderboards.filter(
+            is_admin=True
+        ).exists()
+        
+        if is_admin_somewhere:
+            messages.error(
+                request, 
+                "You cannot leave this competition because you are an administrator of a private leaderboard within it. "
+                "Step down from your admin position or delete the leaderboard first."
+            )
+            return redirect('competition_detail', code=competition.code)
+            
+        competition_registration.delete()
+        messages.success(request, f"You have successfully left the competition: {competition.name}.")
+        return redirect('competition_list')
